da6c45dd8739a6becba5a64c2bf6d7d7f18cb9bd
Javier Darsie <44655727+jadarsie@users.noreply.github.com>
2022-10-03 15:36:18 -0700

    fix: syntax error in Windows VHD script (#41)

--- vhd/packer/configure-windows-vhd.ps1
+++ vhd/packer/configure-windows-vhd.ps1
@@ -130,7 +130,7 @@ function Get-FilesToCacheOnVHD {
             "https://kubernetesartifacts.azureedge.net/kubernetes/v1.24.3/windowszip/v1.24.3-1int.zip"
         );
         "c:\akse-cache\win-vnet-cni\" = @(
-            "https://kubernetesartifacts.azureedge.net/azure-cni/v1.4.32/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.4.32.zip",
+            "https://kubernetesartifacts.azureedge.net/azure-cni/v1.4.32/binaries/azure-vnet-cni-singletenancy-windows-amd64-v1.4.32.zip"
         )
     }
 
