554fe643753cbd293a421a1ae25f46b6e3cd69b0
Javier Darsie <44655727+jadarsie@users.noreply.github.com>
2023-02-14 15:45:46 -0800

    ci: chocolatey workflow (#86)
    * ci: chocolatey workflow
    
    * updated title

---
+++ .choco/aks-engine-azurestack/tools/chocolateyinstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageArgs = @{
+  packageName   = 'aks-engine-azurestack'
+  softwareName  = 'aks-engine-azurestack*'
+  fileType      = 'exe'
+  url64bit      = '_placeholder_'
+  checksum64    = '_placeholder_'
+  checksumType64= 'sha256'
+  unzipLocation = $toolsDir
+}
+
+Install-ChocolateyZipPackage @packageArgs
