ca496b2cd7c906be86df88b0578dfade83a86f3f
Christina Lau <christinalau@microsoft.com>
2023-03-18 23:56:58 -0700

    fix: change k8s v1.25 addon manager MCR image version to 9.1.5

--- pkg/api/k8s_versions.go
+++ pkg/api/k8s_versions.go
@@ -258,7 +258,7 @@ var kubernetesImageBaseVersionedImages = map[string]map[string]map[string]string
 		"1.25": {
 			common.AddonResizerComponentName:  "oss/kubernetes/autoscaler/addon-resizer:1.8.7",
 			common.MetricsServerAddonName:     "oss/kubernetes/metrics-server:v0.5.2",
-			common.AddonManagerComponentName:  "oss/kubernetes/kube-addon-manager:v9.1.6",
+			common.AddonManagerComponentName:  "oss/kubernetes/kube-addon-manager:v9.1.5",
 			common.ClusterAutoscalerAddonName: "oss/kubernetes/autoscaler/cluster-autoscaler:v1.22.1",
 		},
 		"1.24": {
