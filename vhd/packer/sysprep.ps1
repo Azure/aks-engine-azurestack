ff938fd00ff43fc19df518c99a3c40da371f8f78
Hao Fan <hafa@microsoft.com>
2024-05-14 20:17:27 -0700

    test windows clean up

--- vhd/packer/sysprep.ps1
+++ vhd/packer/sysprep.ps1
@@ -32,5 +32,5 @@ $values | ForEach-Object {
 
 # when done clean up
 #while($true) { $imageState = Get-ItemProperty HKLM:\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Setup\\State | Select ImageState; if($imageState.ImageState -ne 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { Write-Output $imageState.ImageState; Start-Sleep -s 10  } else { break } }
-#Get-ChildItem c:\\WindowsAzure -Force | Sort-Object -Property FullName -Descending | ForEach-Object { try { Remove-Item -Path $_.FullName -Force -Recurse -ErrorAction SilentlyContinue; } catch { } }
+Get-ChildItem c:\\WindowsAzure -Force | Sort-Object -Property FullName -Descending | ForEach-Object { try { Remove-Item -Path $_.FullName -Force -Recurse -ErrorAction SilentlyContinue; } catch { } }
 #Remove-Item -Path WSMan:\\Localhost\\listener\\listener* -Recurse
