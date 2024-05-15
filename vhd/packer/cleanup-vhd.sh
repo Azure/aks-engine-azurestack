5699704150cc989bedcfb22db7f60a693e9e4005
Hao Fan <hafa@microsoft.com>
2024-05-15 14:31:56 -0700

    clean up for linux

--- vhd/packer/cleanup-vhd.sh
+++ vhd/packer/cleanup-vhd.sh
@@ -2,6 +2,6 @@
 
 # Cleanup packer SSH key and machine ID generated for this boot
 rm -f /root/.ssh/authorized_keys
-rm -f /home/packer/.ssh/authorized_keys
+#rm -f /home/packer/.ssh/authorized_keys
 rm -f /etc/machine-id
 touch /etc/machine-id
