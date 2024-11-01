481c93ce5c160ad5fe018da4e70d87aea7431b55
Christina Lau <christinalau@microsoft.com>
2024-10-31 19:22:57 -0700

    Use 17763.5329.20243062 windows image

--- pkg/api/azenvtypes.go
+++ pkg/api/azenvtypes.go
@@ -165,7 +165,7 @@ var (
 		ImageOffer:     "aks-windows",
 		ImageSku:       "2019-datacenter-core-azurestack-ctrd",
 		ImagePublisher: "microsoft-aks",
-		ImageVersion:   "17763.5329.20243053",
+		ImageVersion:   "17763.5329.20243062",
 	}
 
 	// WindowsServer2019OSImageConfig is the 'vanilla' Windows Server 2019 image
